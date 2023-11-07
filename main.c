69e5d3395f422efc24e541ba84a7e8b68c2896c9
SachinKaulSJSU <106776301+SachinKaulSJSU@users.noreply.github.com>
2023-11-07 08:47:03 -0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/queue.h>
+#include <stdbool.h>
+
+int time;
+enum state{READY, RUNNING, BLOCKED};
+//implement queue CPU
+
+typedef struct {
+    int time;
+    int childID;
+    int parentID;
+    int* programCounter;
+    int userInteger;
+    int priority;
+    enum state state;
+    int startTime;
+    int cpuTime;
+} PcbBlock;
+
+PcbBlock PcbTable[20];
+
+//The state arrays store indexes of PcbTable
+int ReadyState[20];
+int BlockedState[20];
+int RunningState[1];
+
+int main(){
+    while(true){
+        char userInput;
+        printf("Please enter one of the commands: Q, U, P, T. \n");
+        scanf("%c", userInput);
+        switch(userInput){
+            case 'Q': 
+                //Start Simulated code
+                break;
+            case 'U':
+                //Commander code
+                break;
+            case 'P':
+                //Simulated code?
+                break;
+            case 'T':
+                //Commander code
+                break;
+        }    
+    }
+    return 0;
+}
+
+
+
