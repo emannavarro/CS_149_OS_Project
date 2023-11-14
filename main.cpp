f6d4351bc14db5ef476350709d7e82dfa6b4f66d
aokj08 <104790843+aokj08@users.noreply.github.com>
2023-11-13 16:22:15 -0800

    created a pipe and process manager via fork()

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 #include <stdlib.h>
 #include <sys/queue.h>
 #include <stdbool.h>
+#include <iostream>
+
+#define READ_FD 0
+#define WRITE_FD 1
 
 using namespace std;
 
@@ -30,25 +34,37 @@ int BlockedState[20];
 int RunningState[1];
 
 int main(){
-    while(true){
-        char userInput;
-        printf("Please enter one of the commands: Q, U, P, T. \n");
-        scanf("%c", userInput);
-        switch(userInput){
-            case 'Q': 
-                //Start Simulated code
-                break;
-            case 'U':
-                //Commander code
-                break;
-            case 'P':
-                //Simulated code?
-                break;
-            case 'T':
-                //Commander code
-                break;
-        }    
+    int pip[2];
+    pid_t processManagerPID;
+    if(pipe(pip) == -1) {
+        perror("Failed to create pipe\n");
+        exit(1);
     }
+    processManagerPID = fork();
+    if(processManagerPID == 0) { //We are in process manager
+        //Perform simulation
+    }
+
+    wait(NULL); //Wait for the simulation to end before exiting the program.
+    // while(true){
+    //     char userInput;
+    //     printf("Please enter one of the commands: Q, U, P, T. \n");
+    //     scanf("%c", userInput);
+    //     switch(userInput){
+    //         case 'Q': 
+    //             //Start Simulated code
+    //             break;
+    //         case 'U':
+    //             //Commander code
+    //             break;
+    //         case 'P':
+    //             //Simulated code?
+    //             break;
+    //         case 'T':
+    //             //Commander code
+    //             break;
+    //     }    
+    // }
     return 0;
 }
 
